ee6ab2ae2917f5f895d98095528a7803f732e949
Vic P <vic4key@gmail.com>
2022-03-20 17:28:06 +0700

    Initial

--- PyVE/pyve-help.cmd
+++ PyVE/pyve-help.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 
 ECHO List of commands as the following :
-
-FOR /R %%i IN (pyve-*.cmd) DO (
-	ECHO 	%%~ni
-)
+ECHO.
+ECHO pyve-c        - Create a new python virtual environment.
+ECHO pyve-create   - Create a new python virtual environment.
+ECHO.
+ECHO pyve-a        - Active a python virtual environment.
+ECHO pyve-active   - Active a python virtual environment.
+ECHO.
+ECHO pyve-d        - Deactive a python virtual environment.
+ECHO pyve-deactive - Deactive a python virtual environment.
