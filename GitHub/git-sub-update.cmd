f694c63142868baaa3c977e88b76bbbba6e1e473
Vic P <vic4key@gmail.com>
2022-10-08 17:27:43 +0700

    Initial

--- GitHub/git-sub-update.cmd
+++ GitHub/git-sub-update.cmd
@@ -2,6 +2,6 @@
 
 CALL git-sub-clone
 
-submodule update --progress --init --recursive --force
+git submodule update --progress --init --recursive --force
 rem git submodule foreach --recursive git checkout master
 rem git submodule foreach --recursive git pull
