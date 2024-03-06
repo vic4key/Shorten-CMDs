de24faaaa1d6cd1777c3dd04a4cd4851c2a2488d
Vic P <vic4key@gmail.com>
2024-03-06 23:08:44 +0700

    Initial

--- GitHub/git-pull.cmd
+++ GitHub/git-pull.cmd
@@ -1,3 +1,17 @@
 @ECHO OFF
 
-git pull origin master
+:: N-Args
+
+SET NARGS=0
+FOR %%I IN (%*) DO (
+   SET /A NARGS+=1
+   SET "ARGV[!NARGS!]=%%~I"
+)
+
+:: Status/Differences
+
+IF %NARGS% NEQ 0 (
+	git pull --no-rebase origin %1 %2 %3
+) ELSE (
+  git pull origin master
+)
